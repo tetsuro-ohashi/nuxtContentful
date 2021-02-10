d2ef129222425422d50c52b7333b9f88c2b7ab22
tetsuro-ohashi <ohashi@thisco.jp>
2021-02-11 03:08:29 +0900

    sample json infinite

---
+++ nuxt.config.js
@@ -0,0 +1,69 @@
+const config = require('./.contentful.json')
+
+export default {
+  env: {
+    CTF_SPACE_ID: process.env.CTF_SPACE_ID,
+    CTF_CDA_ACCESS_TOKEN: process.env.CTF_CDA_ACCESS_TOKEN,
+    CTF_PREVIEW_ACCESS_TOKEN: process.env.CTF_PREVIEW_ACCESS_TOKEN,
+    CTF_PERSON_ID: process.env.CTF_PERSON_ID,
+    CTF_BLOG_POST_TYPE_ID: process.env.CTF_BLOG_POST_TYPE_ID
+  },
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'nuxtContentful',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+
+  styleResources: {
+    scss: [
+      '@/assets/scss/app.scss' // 読みませたい変数を記述したscssファイル
+    ]
+  },
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+    { src: '~/plugins/infiniteloading', ssr: false },
+    '~/plugins/markdownit', // 追加
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/bootstrap
+    'bootstrap-vue/nuxt',
+    '@nuxtjs/markdownit'
+  ],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  },
+
+  env: {
+    CTF_SPACE_ID: config.CTF_SPACE_ID,
+    CTF_CDA_ACCESS_TOKEN: config.CTF_CDA_ACCESS_TOKEN,
+    CTF_PERSON_ID: config.CTF_PERSON_ID,
+    CTF_BLOG_POST_TYPE_ID: config.CTF_BLOG_POST_TYPE_ID
+  },
+
+  server: {
+    port: 8000 // デフォルト: 3000
+  },
+}
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-flex>
+    <v-container fluid grid-list-lg class="mt-5">
+      <posts/>
+    </v-container>
+  </v-flex>
+</template>
+
+<script>
+import posts from "~/components/Posts.vue";
+
+export default {
+  components: {
+    posts
+  }
+}
+</script>
