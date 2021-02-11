79d0cfe216b6e039d799a928d61071646cf790b7
tetsuro-ohashi <ohashi@thisco.jp>
2021-02-12 02:30:43 +0900

    base修正

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,7 +34,7 @@ export default {
   },
 
   router: {
-    base: '/nuxtContentful/'
+    // base: '/nuxtContentful/'
   },
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
