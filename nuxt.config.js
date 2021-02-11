f773e02b62e96d226845da5d9dad161359903747
tetsuro-ohashi <ohashi@thisco.jp>
2021-02-12 01:57:46 +0900

    設定変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,6 +33,10 @@ export default {
     ]
   },
 
+  router: {
+    base: '/nuxtContentful/'
+  },
+
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     { src: '~/plugins/infiniteloading', ssr: false },
