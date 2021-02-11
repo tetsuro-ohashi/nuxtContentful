28e59ee9756dcb5c20ecc4b72ed9069b3e1dc0e2
tetsuro-ohashi <ohashi@thisco.jp>
2021-02-12 00:45:13 +0900

    ssr to spa

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,7 @@
 const config = require('./.contentful.json')
 
 export default {
+  ssr: false,
   env: {
     CTF_SPACE_ID: process.env.CTF_SPACE_ID,
     CTF_CDA_ACCESS_TOKEN: process.env.CTF_CDA_ACCESS_TOKEN,
