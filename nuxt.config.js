55064daa7f602897faa066ec43178dfec9510927
tetsuro-ohashi <ohashi@thisco.jp>
2021-02-12 11:42:03 +0900

    github pages設定に変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,8 +21,11 @@ export default {
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/nuxtContentful/favicon.ico' }
     ]
+  },
+  router: {
+    base: '/nuxtContentful/'
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
